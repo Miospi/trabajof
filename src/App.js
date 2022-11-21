1abbca74915d41a5d4599dd5b39efbf8b8c285e6
Miospi <miospi88@gmail.com>
2022-11-20 20:26:50 -0500

    Subida de documento

--- src/App.js
+++ src/App.js
@@ -1,22 +1,187 @@
 import logo from './logo.svg';
 import './App.css';
-
+//hg
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>kd</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="CV Miguel Ospina" />
+    <meta name="author" content="Miguel Ospina Quintero" />
+    <title>CV MIGUEL OSPINA</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    Font Awesome icons (free version)
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            <span class="d-block d-lg-none">Miguel Ospina Quintero</span>
+            <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2"
+                    src="assets/img/WhatsApp Image 2022-10-25 at 12.11.15 PM.jpeg" alt="..." /></span>
         </a>
-      </header>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
+                class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Sobre mi</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experiencia</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Educacion</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Habilidades</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Intereses</a></li>
+                
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="container-fluid p-0">
+        
+        <section class="resume-section" id="about">
+            <div class="resume-section-content">
+                <h1 class="mb-0">
+                    Miguel
+                    <span class="text-primary">Ospina</span>
+                </h1>
+                <div class="subheading mb-5">
+                    Manizales · Caldas, · (316)4550024 ·
+                    <a href="mailto:name@email.com">mospina89350@umanizales.edu.co</a>
+                </div>
+                <p class="lead mb-5">Estudiante de ingeniería en sistemas y telecomunicaciones, con amplia
+                    experiencia en áreas informáticas de: software (Énfasis en mantenimiento
+                    computacional y móvil, lectura simple en programación, gran catálogo de
+                    manejo en software) y hardware (conocimientos de hardware, evaluación de
+                    componentes físicos y mantenimiento). También se tiene experiencia en el
+                    ámbito de contenido multimedia tales como Creación, edición, fotografía,
+                    modelaje y creación 3D en Unity y Vegas Pro
+                    Adicionalmente, se cuenta con amplia experiencia en el ámbito de
+                    comunicaciones sociales e interpersonales, con notorio énfasis en la
+                    comunicación cliente-empleado, además de poseer un buen manejo ante
+                    situaciones y problemas de equipo, con gran facilidad para desenvolverse en
+                    situaciones que requieran de trabajo conjunto.</p>
+                <div class="social-icons">
+                    
+                    <a class="social-icon" href="https://github.com/Miospi"><i class="fab fa-github"></i></a>
+                    <a class="social-icon" href="https://twitter.com/miospi"><i class="fab fa-twitter"></i></a>
+                    
+                </div>
+            </div>
+        </section>
+        <hr class="m-0" />
+        
+        <section class="resume-section" id="experience">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Experiencia</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Tecnologia informatica (T.I)</h3>
+                        <div class="subheading mb-3">Soporte T.I</div>
+                        <p> Se prestaba servicio al personal de ALADINO SALAS DE JUEGOS S.A en el ambito del area imformatica dandole soporte a: 
+                            Servidore, CPU, Celulares, Camaras y gestion y manipulacion de redes. 
+                            de igual modo se maneja proteccion, reguardo y disponibilidad de informacion de la empresa
+                        </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Febrero 2022 - Present</span></div>
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Uno27- Claro Colombia - Min tic</h3>
+                        <div class="subheading mb-3">Asesor de tiempo completo al cliente, en el área de fidelidad.
+                                                     Asesor de tiempo completo al cliente, en área de ventas</div>
+                        <p>Trabajar con el cliente proporcionarle la mejor información ya sea porque
+                            el usuario tiene curiosidad, inquietud con el producto. Adicional a ellos
+                            brindarle veneficios por pertenecer a la compañía. </p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">jul 2021 -sep 2021</span></div>
+                </div>
+                
+                
+            </div>
+        </section>
+        <hr class="m-0" />
+        
+        <section class="resume-section" id="education">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Educación</h2>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div class="flex-grow-1">
+                        <h3 class="mb-0">Universidad de manizales</h3>
+                        <div class="subheading mb-3">Ingeniero en sistemas y telecomunicaciones</div>
+                        <div>Terminado estudios de ingenieria</div>
+                        <p></p>
+                    </div>
+                    <div class="flex-shrink-0"><span class="text-primary">Sep 2020 - Actualidad</span></div>
+                </div>
+
+            </div>
+        </section>
+        <hr class="m-0" />
+        
+        <section class="resume-section" id="skills">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Habilidades</h2>
+                <div class="subheading mb-3">Programar en Languajes & Herramientas</div>
+                <ul class="list-inline dev-icons">
+                    <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-java"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-git"></i></li>
+                    <li class="list-inline-item"><i class="fab fa-python"></i></li>
+                </ul>
+                <div class="subheading mb-3">Habilidad al trabajar</div>
+                <ul class="fa-ul mb-0">
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Buen trabajo en equipo
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        habilidad de producción bajo presion
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Razonamiento critico ante problemas
+                    </li>
+                    <li>
+                        <span class="fa-li"><i class="fas fa-check"></i></span>
+                        Agilidad de solucionamiento de problematicas
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <hr class="m-0" />
+        
+        <section class="resume-section" id="interests">
+            <div class="resume-section-content">
+                <h2 class="mb-5">Intereses</h2>
+                <p>Aparte de un rubro de estudio y aprendisaje, tambien me gusta tener una disponibilidad del tiempo para poder entrenar que es
+                    uno de los pasa tiempos que mas gustan, al igual que leer un libro o salir a caminar y deconectarme del mundo tecnologico durante un tiempo.
+
+                </p>
+              
+            </div>
+        </section>
+        <hr class="m-0" />
+        
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
   );
 }
 
